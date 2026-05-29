ff34986b325a7bee00745924183ffe360e5f0c25
Swara Anand Saoji <swarasaoji@Swaras-MacBook-Air.local>
2026-05-29 15:41:14 +0530

    Prepared FastAPI deployment

--- fastAPI_backend/main.py
+++ fastAPI_backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
@@ -12,9 +13,17 @@ origins = [
     "http://localhost:8000",
     "http://localhost:3000",
 ]
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
